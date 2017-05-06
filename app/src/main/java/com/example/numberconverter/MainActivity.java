9f0904f4f05f350c5d6eeaf50498f0bc076fa436
ngynls <ngyn.ls@gmail.com>
2017-05-05 22:26:25 -0400

    first commit

---
+++ app/src/main/java/com/example/numberconverter/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.numberconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    //Declare elements as member variable
+    EditText inputTxt;
+    EditText outputTxt;
+    Button doIt;
+    Button clear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Create references
+        inputTxt=(EditText) findViewById(R.id.inputs);
+        outputTxt=(EditText) findViewById(R.id.outputs);
+        doIt=(Button) findViewById(R.id.doIt);
+        clear=(Button) findViewById(R.id.clear);
+
+
+        //Set Listeners to Buttons
+        doIt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+            //perform the conversion
+            String str=inputTxt.getText().toString();
+                try{
+                    for(int i=0;i<str.length()-1;i++)
+                    {
+                        if(!str.matches("[01]+"))
+                           throw new Exception();
+                    }
+                    int binNum=Integer.parseInt(str,2);
+                    String bToD=String.valueOf(binNum);
+                    outputTxt.setText(bToD);
+                }
+                catch(Exception e){
+                    outputTxt.setText("WRONG INPUT- try again"); // error message if the input isn't composed of 0 and 1
+                }
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputTxt.setText("");
+                outputTxt.setText("");
+            }
+        });
+    }
+}
